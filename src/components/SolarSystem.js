79a240b6d2f531242bd9225bdcda41c8a0b9d03b
Giuliano <giulianovieiramartins@gmail.com>
2022-09-21 17:29:02 -0300

    sistema solar ok

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import PlanetCard from './PlanetCard';
 import Title from './Title';
+import planets from '../data/planets';
 
 export default class SolarSystem extends Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title Headline="planetas" />
+        {planets.map((planeta, index) => (<PlanetCard
+          key={ index }
+          planetImage={ planeta.image }
+          planetName={ planeta.name }
+        />))}
       </div>
     );
   }
