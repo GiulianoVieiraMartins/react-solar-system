5fa264a18b3f3442942a3e15e834fa25464704f9
Giuliano <giulianovieiramartins@gmail.com>
2022-09-21 18:16:12 -0300

    finalizando

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Title from './Title';
 
 class Header extends React.Component {
   render() {
     return (
       <header>
         <h1>Sistema Solar</h1>
-        {/* <Title headline="Planetas" /> */}
       </header>
     );
   }
